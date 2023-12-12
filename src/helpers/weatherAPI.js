f6cfbcdace8e8c6147a1107223393de3b736d204
Renan Marques <nan.dev01@gmail.com>
2023-12-12 11:26:57 -0300

    Implements searchCities function

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,14 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
+const API_URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=`;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const response = await fetch(`${API_URL}${term}`);
+  const data = await response.json();
+  if (!data.length) {
+    window.alert('Nenhuma cidade encontrada');
+    return data;
+  }
+  return data;
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
