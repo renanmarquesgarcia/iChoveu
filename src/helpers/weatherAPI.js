d16e03d11adfed88fe079e0edcd6ff0d0bbf2305
Renan Marques <nan.dev01@gmail.com>
2023-12-13 09:20:19 -0300

    Implements getWeatherByCity function and adds a feature in handleSearch function

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,16 +1,31 @@
 const TOKEN = import.meta.env.VITE_TOKEN;
-const API_URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=`;
 
 export const searchCities = async (term) => {
+  const API_URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=`;
+
   const response = await fetch(`${API_URL}${term}`);
   const data = await response.json();
+
   if (!data.length) {
     window.alert('Nenhuma cidade encontrada');
     return data;
   }
+
   return data;
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+export const getWeatherByCity = async (cityURL) => {
+  const API_URL = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=`;
+
+  const response = await fetch(`${API_URL}${cityURL}`);
+  const { current, location } = await response.json();
+
+  return {
+    temp: current.temp_c,
+    condition: current.condition.text,
+    icon: current.condition.icon,
+    country: location.country,
+    name: location.name,
+    url: cityURL,
+  };
 };
